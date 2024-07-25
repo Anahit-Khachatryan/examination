22cb4b1c3109ddb53c54b621d9b7838e24eeba7d
VOLO-DC\anahit.khachatryan <anahit.khachatryan@volo.global>
2024-07-25 18:39:47 +0400

    async programming

---
+++ async-programming/async.js
@@ -0,0 +1,7 @@
+console.log(1);
+setTimeout(() => {
+    console.log(2)
+},0)
+console.log(3)
+
+//katarvelu e 1,3,2 - tekuz 0 e bayc asuma timer miacru, erb execut lini, es callbacky kkatares, callstackum function ka, ani erb verjana naylu e callstacky datarka, dnelu e queic stack
