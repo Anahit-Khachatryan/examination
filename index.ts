6d58c517ce91f079a966e1f034d65a1cb077377b
Anahit-Khachatryan <44209605+Anahit-Khachatryan@users.noreply.github.com>
2024-10-03 16:29:34 +0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -19,6 +19,7 @@
 // 12. importnery vor ham import ham loadChildereni nmana arvum, dranic el harcrecin
 //13. WeakMap, WeakSet, Set, Map
 //14.Authinticate u authentication
+//15. Git-rebase, branching, 
 // console.log(+obj) - sa ughaki em grum, vonc anel vor + dnel u tpi orinak 5, erb grum enk +, inky takic kanchum e Symbol.toPrimitive fucntiony dra hamar karank objecti mej da avelacnenk u valuen veradardznenk
 
 
